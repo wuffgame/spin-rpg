66a4aef8fcdb6412cc78a8ce9b0cffe38a876037
wuffgame <kacper5555544444@gmail.com>
2026-06-23 00:37:53 +0200

    added main menu

---
+++ scenes/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/TEST.tscn")
+
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
